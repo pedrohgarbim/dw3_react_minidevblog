abbe07764e008c79c05ada61cddd79e53c79bb93
pedrohgarbim <phg.garbim@gmail.com>
2024-03-12 12:06:12 -0300

    Aula1203-Eventos, Componontes e Renderização

--- basic/basictools/src/App.jsx
+++ basic/basictools/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
+import Form from './components/Form/Form'
 function App() {
   return (
     <>
     <NavBar></NavBar>
+    <Form></Form>
     <Footer></Footer>
     </>
   )
