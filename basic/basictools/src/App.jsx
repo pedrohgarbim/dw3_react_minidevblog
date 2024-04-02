8bfe02216319f16e061a483f6a3c2824c92de276
pedrohgarbim <phg.garbim@gmail.com>
2024-04-02 12:34:16 -0300

    aula0204 Styles e Props

--- basic/basictools/src/App.jsx
+++ basic/basictools/src/App.jsx
@@ -4,7 +4,17 @@ import Footer from './components/Footer/Footer'
 import Form from './components/Form/Form'
 import FormHooks from './components/Hooks/FormHooks'
 import ConditionalRender from './components/ConditionalRender/ConditionalRender'
+import ShowUserName from './components/ShowUserName/ShowUserName'
+import {useState} from 'react'
 function App() {
+  const [user, setUser] = useState([
+    {active: true, userName: 'PEDRO H. GARBIM'},
+    {active: true, userName: 'COMANDANTE DA GUERRA'},
+    {active: true, userName: 'GENERAL UNIVERSAL'},
+    {active: true, userName: 'COMANDO FORTIFICADO'},
+    {active: true, userName: 'GENERAL INFERNAL'},
+    {active: true, userName: 'MARECHAL 10 ESTRELAS'}
+  ])
   return (
     <>
     <NavBar></NavBar>
@@ -12,6 +22,7 @@ function App() {
     <Footer></Footer>
     <FormHooks></FormHooks>
     <ConditionalRender></ConditionalRender>
+    <ShowUserName list={user}></ShowUserName>
     </>
   )
 }
