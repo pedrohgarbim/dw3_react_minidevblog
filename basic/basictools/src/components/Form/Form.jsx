e82ecba1c79e2935db0a3f29cac2eb35546b56e3
pedrohgarbim <phg.garbim@gmail.com>
2024-04-02 10:43:01 -0300

    AULA 2603 e 0204 Renderizadores com Hooks

--- basic/basictools/src/components/Form/Form.jsx
+++ basic/basictools/src/components/Form/Form.jsx
@@ -1,47 +1,39 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 var teste = 8
 const Form = () => {
-    const handleMyEvent =() =>{
-        console.log()
-        console.log("Ativou o evento")
+    const handleMyEvent = (e) => {
+        console.log(e)
+        console.log("Ativou o Evento!")
     }
-    const renderButton = (x) =>{
-        if(x){
-            return <h1>Renderizou bonitinho</h1>
-        } else{
-            return <h1>Renderizou lindinho</h1>
-        }
-    }
-  return (
-    <>
-    <div>
-        <div>
-            <button onClick={handleMyEvent}>Clique Aqui</button>
-        </div>
-        <div>
-            <button onClick={()=> {console.log("Yes! apertou em mim")}}>Clique em mim tambem</button>
-        </div>
-        <div>
-            <button onClick={()=>{
-                if (true){
-                    console.log("isso nao deveria acontecer")
-                }
-            }}>Sera que voce clicaria aqui?</button>
-        </div>
-        <div>
-            {renderButton(true)}
-            {renderButton(false)}
-        </div>
-    </div>
-    <div>
-        <strong>Valor:</strong>{teste}
-        <button onClick={() => {teste = 12; console.log(teste)}}>Aperte e mude</button>
-        <strong>Valor:</strong>{teste}
-    </div>
-    </>
-
-  )
+    const [x, setX] = useState(true)
+    return (
+        <>
+            <div>
+                <div>
+                    <button onClick={handleMyEvent}>Clique Aqui</button>
+                </div>
+                <div>
+                    <button onClick={() => { console.log("Yes! Apertou em mim.") }}>Clique em mim também!</button>
+                </div>
+                <div>
+                    <button onClick={() => {
+                        if (true) {
+                            console.log("Isso não deveria acontecer!")
+                        }
+                    }}>Será que você clicaria aqui?</button>
+                </div>
+                <div>
+                    <h1>{x ? 'Renderizou' : 'Deu ruim não foi'}</h1>
+                    <button onClick={() => setX(!setX)}>Valida x</button>
+                </div>
+            </div>
+            <div>
+                <strong>Valor:</strong>{teste}
+                <button onClick={() => { teste = 12; console.log(teste) }}>Aperte e mude</button>
+                <strong>Valor:</strong>{teste}
+            </div>
+        </>
+    )
 }
 
 export default Form
